a4068ea5e5ebd51d2dc99c866a82cb1a39a20e60
Bimsara <bimsara.anaththapathiranage@tuni.fi>
2026-07-31 00:03:42 +0300

    sds

--- EdenRequest.Api/DTO/CreateExtraDirtyReportDto.cs
+++ EdenRequest.Api/DTO/CreateExtraDirtyReportDto.cs
@@ -5,18 +5,14 @@ namespace EdenRequest.Api.DTOs
 {
     public class CreateExtraDirtyReportDto
     {
-        [Required(ErrorMessage = "Room number is required.")]
-        [FromForm(Name = "roomNumber")]
+        [Required]
         public string RoomNumber { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "ReportedById is required.")]
-        [FromForm(Name = "reportedById")]
+        [Required]
         public int ReportedById { get; set; }
 
-        [FromForm(Name = "notes")]
         public string? Notes { get; set; }
 
-        [FromForm(Name = "files")]
-        public List<IFormFile> Files { get; set; } = new List<IFormFile>();
+        public List<IFormFile> Files { get; set; } = new();
     }
 }
