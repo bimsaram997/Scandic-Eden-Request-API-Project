d8702c82ee5d18badf56d44525b741b34dd59f9a
Bimsara <bimsara.anaththapathiranage@tuni.fi>
2026-07-31 08:26:46 +0300

    fix

--- EdenRequest.Api/DTO/CreateExtraDirtyReportDto.cs
+++ EdenRequest.Api/DTO/CreateExtraDirtyReportDto.cs
@@ -1,16 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 
-namespace EdenRequest.Api.DTOs
+namespace EdenRequest.Api.DTOs // (adjust namespace to match yours)
 {
     public class CreateExtraDirtyReportDto
     {
         [Required(ErrorMessage = "Room number is required.")]
-        [FromForm(Name = "roomNumber")]
+        [FromForm(Name = "roomNumber")] // <--- THIS TELLS .NET TO BIND 'roomNumber'
         public string RoomNumber { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "ReportedById is required.")]
-        [FromForm(Name = "reportedById")]
+        [FromForm(Name = "reportedById")] // <--- Binds 'reportedById'
         public int ReportedById { get; set; }
 
         [FromForm(Name = "notes")]
