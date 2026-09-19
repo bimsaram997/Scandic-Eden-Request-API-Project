98964d751c57bda1627d7b5b677a340dabfe91e5
Bimsara <bimsara.anaththapathiranage@tuni.fi>
2026-09-19 17:50:42 +0300

    address added

--- EdenRequest.Api/Data/Employee.cs
+++ EdenRequest.Api/Data/Employee.cs
@@ -4,8 +4,13 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public DateTime? AddedDate { get; set; }
+
 
         public string Role { get; set; } = "Housekeeper"; 
 
